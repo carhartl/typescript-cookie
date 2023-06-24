carhartl/typescript-cookie

commit ce66c8cef19afc02572e829623148fe41ade7c95

Eliminate ESLint warnings

types/index.ts
@@ -1,11 +1,14 @@
-export interface CookieAttributes {
+export interface WellKnownCookieAttributes {
   path?: string
   domain?: string
   expires?: number | Date
   sameSite?: 'strict' | 'Strict' | 'lax' | 'Lax' | 'none' | 'None'
   secure?: boolean
-  [property: string]: any
 }
+
+export type CookieAttributes =
+  | WellKnownCookieAttributes
+  | { [property: string]: string | undefined }
 
 export type CookieAttributesConfig = Readonly<CookieAttributes>
 

src/api.ts
@@ -13,8 +13,10 @@ import {
   encodeValue as defaultValueEncoder
 } from './codec'
 
+type AsUnionFromInterface<T> = T[keyof T]
+
 function stringifyAttributes(
-  attributes: CookieAttributes & { expires?: any }
+  attributes: CookieAttributes | { expires: string }
 ): string {
   // Copy incoming attributes as to not alter the original object..
   attributes = Object.assign({}, attributes)
@@ -23,12 +25,16 @@ function stringifyAttributes(
     attributes.expires = new Date(Date.now() + attributes.expires * 864e5)
   }
   if (attributes.expires != null) {
-    attributes.expires = attributes.expires.toUTCString()
+    attributes.expires = (attributes.expires as Date).toUTCString()
   }
 
   return (
     Object.entries(attributes)
-      .filter(([key, value]: [string, any]) => value != null && value !== false)
+      .filter(
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        ([key, value]: [string, AsUnionFromInterface<CookieAttributes>]) =>
+          value != null && value !== false
+      )
       // Considers RFC 6265 section 5.2:
       // ...
       // 3.  If the remaining unparsed-attributes contains a %x3B (";")
@@ -53,7 +59,7 @@ function get<T extends string | undefined, U>(
   decodeName: Decoder<string>
 ): GetReturn<T, U> {
   const scan = /(?:^|; )([^=]*)=([^;]*)/g
-  const jar: any = {}
+  const jar: { [property: string]: U } = {}
   let match
   while ((match = scan.exec(document.cookie)) != null) {
     try {
@@ -67,7 +73,7 @@ function get<T extends string | undefined, U>(
     }
   }
 
-  return name != null ? jar[name] : jar
+  return (name != null ? jar[name] : jar) as GetReturn<T, U>
 }
 
 export const DEFAULT_CODEC: CookieCodecConfig<

test/codec.test.ts
@@ -2,7 +2,7 @@
 
 import { decodeName, decodeValue, encodeName, encodeValue } from '../src/codec'
 
-const NAME_DISALLOWED_CHARS_INPUT_OUTPUT: any = {
+const NAME_DISALLOWED_CHARS_INPUT_OUTPUT: { [key: string]: string } = {
   '(': '%28',
   ')': '%29',
   '<': '%3C',
@@ -24,7 +24,7 @@ const NAME_DISALLOWED_CHARS_INPUT_OUTPUT: any = {
   '\t': '%09'
 }
 
-const VALUE_DISALLOWED_CHARS_INPUT_OUTPUT: any = {
+const VALUE_DISALLOWED_CHARS_INPUT_OUTPUT: { [key: string]: string } = {
   ' ': '%20',
   '"': '%22',
   ',': '%2C',

commit f80d5ab821d0df773743f22ea6f948267c309e7b

Allow boolean again for custom cookie attributes
So that such attributes without a value are possible; fixing regression
introduced in ce66c8c.

types/index.ts
@@ -8,7 +8,7 @@ export interface WellKnownCookieAttributes {
 
 export type CookieAttributes =
   | WellKnownCookieAttributes
-  | { [property: string]: string | undefined }
+  | { [property: string]: string | boolean | undefined }
 
 export type CookieAttributesConfig = Readonly<CookieAttributes>
 